4c0ea2a8053a8d8ead357dc96f08c6bf9f604bc7
Azania Dev <azaniam04@gmail.com>
2022-05-29 18:13:39 +0200

    ref: fixed the types

--- pages/last-launch.tsx
+++ pages/last-launch.tsx
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
-import { gql } from "@apollo/client";
-import { client } from "./_app";
-
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { SPACEX_LATEST_LAUNCH } from "../graphql/queries";
 import NavBar from "../components/NavBar/navbar";
 import Card from "../components/UI/card";
+import Button from "../components/UI/button";
+import Loader from "../components/UI/loader";
+
+interface ILASTLAUNCH {
+  launchLatest: {
+    details?: string;
+  };
+}
 
 const LastLaunch = () => {
-  const [launchDetails, setLaunchDetails] = useState<any>({});
+  const [launchDetails, setLaunchDetails] = useState<
+    ILASTLAUNCH | null | undefined
+  >();
 
-  client
-    .query({
-      query: gql`
-        query {
-          launchLatest {
-            details
-          }
-        }
-      `,
-    })
-    .then((result) => setLaunchDetails(result.data.launchLatest));
+  const { loading, data, error } = useQuery(SPACEX_LATEST_LAUNCH);
+
+  useEffect(() => {
+    if (data) {
+      setLaunchDetails(data);
+    }
+  }, [data]);
+
+  if (loading) {
+    return <Loader />;
+  }
+  if (error) {
+    return <div>Error fetching data</div>;
+  }
 
   return (
     <>
@@ -27,15 +39,16 @@ const LastLaunch = () => {
         <div className="font-bold text-center">
           <h1>Last Rocket Launch</h1>
         </div>
-        {launchDetails.details === null ? (
+        {launchDetails && launchDetails.launchLatest.details == null ? (
           <div className="flex flex-col items-center">
             <img className="w-56" src="/icons/No_data.svg" alt="not found " />
             <p>No details found :(</p>
           </div>
         ) : (
-          <p>{launchDetails.details}</p>
+          <p>{launchDetails && launchDetails.launchLatest.details}</p>
         )}
       </Card>
+      <Button />
     </>
   );
 };
