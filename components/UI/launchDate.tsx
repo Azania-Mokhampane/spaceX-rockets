fe803ab18d975b3f903975f879dc688a140cdee7
Azania Dev <azaniam04@gmail.com>
2022-05-29 08:32:17 +0200

    feat: responsive home page

--- components/UI/launchDate.tsx
+++ components/UI/launchDate.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const LaunchDate = () => {
-  return <div>LaunchDate</div>;
+const LaunchDate = (props: any) => {
+  const setDate = new Date(props.date);
+  const year = setDate.getFullYear();
+  const month = setDate.toLocaleString("en-US", { month: "long" });
+  const day = setDate.getDate();
+
+  return (
+    <div className="flex gap-2 items-center justify-center">
+      <div>{day}</div>
+      <div>{month}</div>
+      <div>{year}</div>
+    </div>
+  );
 };
 
 export default LaunchDate;
