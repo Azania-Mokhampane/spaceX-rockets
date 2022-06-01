70ccd9e79e76b40dd7104e129ab62986222cf022
Azania Dev <azaniam04@gmail.com>
2022-06-01 22:58:32 +0200

    improve performance

--- pages/next-launch.tsx
+++ pages/next-launch.tsx
@@ -1,24 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import { SPACEX_NEXT_LAUNCH } from "../graphql/queries";
+import { SPACEX_LAUNCHES } from "../graphql/queries";
 import NavBar from "../components/NavBar/navbar";
 import Card from "../components/UI/card";
 import Button from "../components/UI/button";
 import Loader from "../components/UI/loader";
 import Image from "next/image";
-
-interface INEXTLAUNCH {
-  launchNext: {
-    details?: string;
-  };
-}
+import { IROCKETS } from "./types/index";
 
 const NextLaunch = () => {
   const [launchDetails, setLaunchDetails] = useState<
-    INEXTLAUNCH | null | undefined
+    IROCKETS | null | undefined
   >();
 
-  const { loading, data, error } = useQuery(SPACEX_NEXT_LAUNCH);
+  const { loading, data, error } = useQuery(SPACEX_LAUNCHES);
 
   useEffect(() => {
     if (data) {
